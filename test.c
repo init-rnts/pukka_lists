331a778c037f46edf2f103bc70a0cc7e1fe6f853
init-rnts <fatihyazicioglu90@gmail.com>
2023-05-16 16:44:10 +0300

    Update

--- test.c
+++ test.c
@@ -1,41 +1,20 @@
-#include "pukka.h"
+#include <stdio.h>
+#include <conio.h>
 #include "pukka2.h"
+
+struct x {
+    int i;
+    double d;
+} x;
 
 int main () {
 
-    pukkaList* sample = createPukkaList();
+    pukkaList sample = createPukkaList(x);
 
-    pushPukka(sample, 5);
-
-    int* ph1 = sample->headNode->data;
-    int* pe1 = sample->endNode->data;
-    struct Node* next = sample->endNode;
-
-    printf("%d\n", *ph1);
-    printf("%d\n", *pe1);
-    printf("%p\n", next->nextNode);
-    printf("%d\n\n", sample->length);
-
-    pushPukka(sample, 10);
-
-    int* ph2 = sample->headNode->data;
-    int* pe2 = sample->endNode->data;
-
-    printf("%d\n", *ph2);
-    printf("%d\n", *pe2);
-    printf("%p\n", next->nextNode);
-    printf("%d\n\n", sample->length);
-
-    pushPukka(sample, 15);
-
-    int* ph3 = sample->headNode->data;
-    int* pe3 = sample->endNode->data;
-
-    printf("%d\n", *ph3);
-    printf("%d\n", *pe3);
-    printf("%d\n\n", sample->length);
-
-
+    printf("%d\n", sample.element_size);
+    printf("%d\n", sample.length);
+    printf("%p\n", sample.headNode);
+    printf("%p\n\n", sample.endNode);
 
     getch();
     return 0;
