e27b262e696279bbbc3a893ac82120fc979c787c
init-rnts <fatihyazicioglu90@gmail.com>
2023-06-12 16:58:53 +0300

    Update

--- pukka2.h
+++ pukka2.h
@@ -1,15 +1,11 @@
 #pragma once
 #ifndef _PUKKA_H
 #define _PUKKA_H
+
 #include <stdlib.h>
-#include <stdio.h>
-#include <assert.h>
-#include <stdbool.h>
-#include <string.h>
 
-#define STRINGIFY_HELPER(x) #x
-
-#define createPukkaList(T) _Static_assert(T>0, "Error")
+#define createPukkaList(T) createPukkaListInternal(sizeof(T))
+#define pushPukka(X, Y) _Generic((Y), int: pushPukkai, default: pushPukkaDefault)(X, Y)
 
 //Defining node of the list which contains a pointer to data to be added and a pointer to next node of the list.
 
@@ -30,9 +26,11 @@ typedef struct PUKKA_LIST {
 
 } pukkaList;
 
-//Function to create a pukkaList
+//Function to create a pukkaList which actually is a pointer to a pukkaList.
 
 pukkaList* createPukkaListInternal(size_t element_size) {
+
+	//Allocate memory for the list and define pointers of the list as null.
 
 	pukkaList* pl = (pukkaList*) malloc(sizeof(pukkaList));
 	pl->headNode = NULL;
@@ -40,7 +38,70 @@ pukkaList* createPukkaListInternal(size_t element_size) {
 	pl->element_size = element_size;
 	pl->length = 0;
 
-	return pl; //Return the list.
+	return pl; //Return the pointer of the allocated memory.
+
+}
+
+void pushPukkai(pukkaList* pl, int i) {
+
+	//Allocate memory for the data added to list and write the argument passed to function to allocated memory.
+
+	int* iptr = (int*) malloc(sizeof(int));
+	*iptr = i;
+
+	//Allocate memory for the new node and then define the node's data member as the pointer above and define 
+	//the node's nextNode member as null
+
+	struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
+	newNode->data = iptr;
+	newNode->nextNode = NULL;
+
+	//Define the nextNode member of the pukkalist's end node as created node.Then add the created new node at 
+	//the end of the given pukkalist. 
+
+	if (pl->length == 0) {
+
+		pl->headNode = newNode;
+		pl->endNode = newNode;
+		pl->length++;
+
+	} else {
+
+		struct Node* nptr = pl->endNode;
+		nptr->nextNode = newNode;
+		pl->endNode = newNode;
+		pl->length++;
+
+	}
+
+}
+
+void pushPukkaDefault(pukkaList* pl, void* data) {
+
+	//Allocate memory for the new node and then define the node's data member as the pointer which given as argument  
+	//and define the node's nextNode member as null
+
+	struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
+	newNode->data = data;
+	newNode->nextNode = NULL;
+
+	//Define the nextNode member of the pukkalist's end node as created node.Then add the created new node at 
+	//the end of the given pukkalist. 
+
+	if (pl->length == 0) {
+
+		pl->headNode = newNode;
+		pl->endNode = newNode;
+		pl->length++;
+
+	} else {
+
+		struct Node* nptr = pl->endNode;
+		nptr->nextNode = newNode;
+		pl->endNode = newNode;
+		pl->length++;
+
+	}
 
 }
 
--- test.c
+++ test.c
@@ -2,19 +2,23 @@
 #include <conio.h>
 #include "pukka2.h"
 
-struct x {
+typedef struct x {
     int i;
     double d;
 } x;
 
 int main () {
 
-    pukkaList* sample = createPukkaList(0);
+    pukkaList* sample = createPukkaList(x);
 
     printf("%d\n", sample->element_size);
     printf("%d\n", sample->length);
     printf("%p\n", sample->headNode);
     printf("%p\n\n", sample->endNode);
+
+    x abc = {15, 5.2}
+
+    pushPukka(sample, abc);
 
     getch();
     return 0;
